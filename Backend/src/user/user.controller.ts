7f1960398d91ec2b28b273c779dc1170e8149ad8
VISHNU M R <vishnumr321k@gmail.com>
2025-08-31 13:56:29 +0530

    Crete the controller and service of user and add create user

--- Backend/src/user/user.controller.ts
+++ Backend/src/user/user.controller.ts
@@ -1,6 +1,15 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller , Get, Post} from "@nestjs/common";
+import { UserSchema } from "./user.schema";
+import { UserService } from "./user.service";
+import { CreateUserDto } from "./dto/user-create.dto";
 
-@Controller()
+
+@Controller('user')
 export class UserController{
-    
-}
+    constructor(private readonly UserService: UserService){}
+
+    @Post('create')
+    createUser(@Body() createUserDto: CreateUserDto){
+        return this.UserService.createUser(createUserDto);  
+    };
+};
--- Backend/src/user/user.service.ts
+++ Backend/src/user/user.service.ts
@@ -1,7 +1,24 @@
-import {Injectable } from "@nestjs/common";
+import {BadRequestException, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
+import { User, UserDocument} from "./user.schema";
+import { Model } from "mongoose";
+import { emit } from "process";
+import { CreateUserDto } from "./dto/user-create.dto";
+
 
 @Injectable()
-export class UserSirvice{
-   
-}
+export class UserService{
+   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>){};
+
+   async createUser(dto: CreateUserDto): Promise<User>{
+        // Checking user is already exit or not;
+        const userExist = await this.userModel.findOne({email: dto.email});
+        if(userExist){
+            throw new BadRequestException('The user is alredy Exist...😶‍🌫️');
+        };
+
+        // if the user is not in the db we can create one;
+        const user = await this.userModel.create(dto);
+        return user.save();
+   };
+};
